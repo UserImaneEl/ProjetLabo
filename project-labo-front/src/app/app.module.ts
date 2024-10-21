6bbde996e16c04fc5fd695cdf7fd7b37158f6c7a
imane <elhallamimane4@gmail.com>
2024-10-21 23:22:17 +0100

    angular project5

--- project-labo-front/src/app/app.module.ts
+++ project-labo-front/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponentComponent } from './Components/test-component/test-component.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { MessageService } from './services/message.service'; // Assurez-vous que ce chemin est correct
 
 @NgModule({
   declarations: [AppComponent, TestComponentComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HttpClient],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [MessageService],
   bootstrap: [AppComponent],
 })
